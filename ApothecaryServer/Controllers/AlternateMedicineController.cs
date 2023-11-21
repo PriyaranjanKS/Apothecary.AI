bdda9481353cfeeb66ca2267bf89a80e9676166f
kspriyaranjan <priyaranjan.ks@hotmail.com>
2023-11-21 08:05:39 +0530

    Alternate Medicine Suggestions V1

---
+++ ApothecaryServer/Controllers/AlternateMedicineController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AlternateMedicineController : ControllerBase
+{
+    [HttpPost("get-suggestion")]
+    public async Task<IActionResult> GetAlternateMedicineSuggestion([FromBody] MedicineRequest request)
+    {
+        try
+        {
+            // URL for your Power Automate flow
+            var powerAutomateUrl = "https://prod-175.westus.logic.azure.com:443/workflows/9f92056f6efe4c2b8bc06d58b0f7d423/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=y2325tqGlUG3GGSUrnq_FGhYrw9Cqf_KAPnzzMKAz_I";
+
+            // Create a new HttpClient instance
+            using var httpClient = new HttpClient();
+
+            var requestData = new
+            {
+                MedicineName = request.MedicineName
+            };
+
+            var content = new StringContent(JsonConvert.SerializeObject(requestData), Encoding.UTF8, "application/json");
+
+            // Sending a POST request to the Power Automate flow
+            var response = await httpClient.PostAsync(powerAutomateUrl, content);
+
+
+            if (response.IsSuccessStatusCode)
+            {
+                // Read the response content and deserialize it
+                var responseContent = await response.Content.ReadAsStringAsync();
+                var alternateMedicineSuggestion = JsonConvert.DeserializeObject<AlternateMedicineResponse>(responseContent);
+
+
+                return Ok(alternateMedicineSuggestion);
+            }
+            else
+            {
+                // If the response is not successful, return an error message
+                return StatusCode((int)response.StatusCode, "Failed to fetch the alternate medicine suggestion.");
+            }
+        }
+        catch (Exception ex)
+        {
+            // If an exception occurs, return a 500 status code with the exception message
+            return StatusCode(500, $"An error occurred: {ex.Message}");
+        }
+    }
+
+    // Helper class for the response
+    public class MedicineRequest
+    {
+        public string MedicineName { get; set; }
+    }
+    private class AlternateMedicineResponse
+    {
+        public string AlternateMedicine { get; set; }
+    }
+}
