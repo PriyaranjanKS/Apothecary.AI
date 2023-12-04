f7365abb17cdfaf8939cb6beea493783a62eae81
kspriyaranjan <priyaranjan.ks@hotmail.com>
2023-12-04 16:39:11 +0530

    UAT Fixes

--- ApothecaryServer/Controllers/MedicinesController.cs
+++ ApothecaryServer/Controllers/MedicinesController.cs
@@ -3,9 +3,10 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Text;
 
 
-    [Route("api/[controller]")]
+[Route("api/[controller]")]
     [ApiController]
     public class MedicinesController : ControllerBase
     {
@@ -38,6 +39,43 @@ using System.Collections.Generic;
         {
             return StatusCode(500, $"An error occurred while fetching medicines: {ex.Message}");
         }
+    }
+
+    [HttpPost("getunitprice")]
+    public async Task<IActionResult> GetUnitPrice([FromBody] MedicineRequestUnitPrice request)
+    {
+        var powerAutomateUrl = "https://prod-105.westus.logic.azure.com:443/workflows/f1d14728d08747d4951857368f188869/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=pdvaGipJu3WGrOZi1AUki2blKeC930O_togbWz87eJ8"; // Replace with your actual Power Automate URL
+
+        try
+        {
+            using var httpClient = new HttpClient();
+            var jsonRequest = JsonConvert.SerializeObject(request);
+            var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+            var response = await httpClient.PostAsync(powerAutomateUrl, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                var responseData = JsonConvert.DeserializeObject<Dictionary<string, decimal>>(jsonResponse);
+
+                return Ok(responseData);
+            }
+            else
+            {
+                // Handle the case where the Power Automate call is not successful
+                return StatusCode((int)response.StatusCode, "Failed to retrieve unit price");
+            }
+        }
+        catch (Exception ex)
+        {
+            // Handle exceptions
+            return StatusCode(500, $"Internal server error: {ex.Message}");
+        }
+    }
+    public class MedicineRequestUnitPrice
+    {
+        public string MedicineName { get; set; }
     }
 }
 
